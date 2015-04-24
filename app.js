04c2acf56e6689f6e00f88b41905c2c74c3599cd
BaiYinSilver <silvervista@gmail.com>
2015-04-24 00:55:23 -0700

    Tutorial Node.js web app using the Azure Table Service

---
+++ app.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies.
+ */
+
+var azure = require('azure-storage');
+var nconf = require('nconf');
+nconf.env()
+     .file({ file: 'config.json'});
+var tableName = nconf.get("TABLE_NAME");
+var partitionKey = nconf.get("PARTITION_KEY");
+var accountName = nconf.get("STORAGE_NAME");
+var accountKey = nconf.get("STORAGE_KEY");
+
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var TaskList = require('./routes/tasklist');
+var Task = require('./models/task');
+var task = new Task(azure.createTableService(accountName, accountKey), tableName, partitionKey);
+var taskList = new TaskList(task);
+
+app.get('/', taskList.showTasks.bind(taskList));
+app.post('/addtask', taskList.addTask.bind(taskList));
+app.post('/completetask', taskList.completeTask.bind(taskList));
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
