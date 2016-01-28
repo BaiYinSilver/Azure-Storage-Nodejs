48c8e8bf331ab7dbd3689a751da14543adcf4330
Yin Bai <silvervista@gmail.com>
2016-01-28 00:02:02 -0800

    Update dependencies - install new modules

--- app.js
+++ app.js
@@ -25,20 +25,12 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/task3-1', express.static(__dirname + '/public'));
 app.use('/task3-2', express.static(__dirname + '/public/task3'));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 var TaskList = require('./routes/tasklist');
 var Task = require('./models/task');
